a3d19f152a9e2f4d66359286a6cdb498a33eac50
william laplante <williamlaplante123@gmail.com>
2020-10-25 15:32:02 -0400

    created map maker scripts for NN input

--- code/scripts/make_ns-maps.py
+++ code/scripts/make_ns-maps.py
@@ -3,21 +3,20 @@ sys.path.append('../')
 from gaussian_fluctuations import generate_gaussian
 from array import array
 import numpy as np
+
 #Note: ns-maps stand for no signal maps. This means that the maps produced by this script have no cosmic string signal in them, by construction.
 
 path = r"./output/ns-maps/" #We dump all the produced maps in the ./output/ns-maps folder.
 
-N = 10 #number of maps to be made.
-
+N = 100 #number of maps to be made.
 
 def main():
     for i in range(N):
         m = generate_gaussian()
-        filename = "ns-map"+str(i)+".bin"
+        filename = "ns-map"+str(i)+".npy"
+        with open(path+filename, 'wb') as f:
+        	np.save(f, m)
 
-        with open(path+filename, "wb") as f:
-            array('d', m).tofile(f)
-            
-            
+        
 if __name__ == '__main__':
     main()
