16bbba493e866130dcba213c12e200221a97781a
williamlaplante <williamlaplante123@gmail.com>
2021-06-18 19:34:55 -0400

    modified data generating scripts and CNN algo

--- code/scripts/make_noise_maps.py
+++ code/scripts/make_noise_maps.py
@@ -2,27 +2,49 @@ import sys
 sys.path.append('../maps_generator/')
 from gaussian_fluctuations import generate_gaussian
 import numpy as np
+import re
+import os
 
-#Note: ns-maps stand for no signal maps. This means that the maps produced by this script contain no cosmic string signal.
+#Note: ns-maps stand for no signal maps. So the maps produced by this script contain no cosmic string signal.
 
 #The maps go in the data folder.
-path_1 = r"../../data/ns-maps/" #The no-signal maps
-path_2 = r"../../data/ws-maps/noise/" #The noise part of the with-signal maps
+path_ns_maps = r"../../data/ns-maps/" #The no-signal maps
+path_ws_maps_noise = r"../../data/ws-maps/noise/" #The noise part of the with-signal maps
 
-N = 100 #number of maps to be made.
+M = 100 #number of maps to be made.
+
+def max_file_num(path):
+    files = os.listdir(path)
+    if len(files)==0 : 
+        return 0
+    else : 
+        start = max([int(re.findall('[0-9]+', file)[0]) for file in files])+1
+    
+    return start
+
 
 def main():
-    for i in range(N):
-        m1 = generate_gaussian()
-        m2 = generate_gaussian()
+    #we start with the no signal maps
+    start = max_file_num(path_ns_maps)
+    
+    for i in range(start, start+M):
+        m = generate_gaussian()
         
         filename = "ns-map"+str(i)+".npy"
-        with open(path_1+filename, 'wb') as f:
-            np.save(f, m1)
+        with open(path_ns_maps+filename, 'wb') as f:
+            np.save(f, m)
+       
+    
+    
+    #We then do the noise part of the signal maps
+    start = max_file_num(path_ws_maps_noise)
+    
+    for i in range(start, start+M):
+        m = generate_gaussian()
         
         filename = "noise"+str(i)+".npy"
-        with open(path_2+filename, 'wb') as f:
-            np.save(f, m2)
+        with open(path_ws_maps_noise+filename, 'wb') as f:
+            np.save(f, m)
         
          
 
--- code/scripts/make_signal_maps.py
+++ code/scripts/make_signal_maps.py
@@ -2,16 +2,33 @@ import sys
 sys.path.append('../maps_generator/')
 from scaling_solution_signal import generate_signal
 import numpy as np
+import re
+import os
 
-#Note: ws-maps stand for with signal maps. This means that the maps produced by this script contain a cosmic string signal.
 
-path = r"../../data/ws-maps/signal/" #We dump all the produced signal maps in the ./output/ws-maps/signal/ folder.
+#Note: ws-maps stand for with signal maps. So this script produces maps containing a cosmic string signal.
 
-N = 100 #number of maps to be made.
+path = r"../../data/ws-maps/signal/" 
+
+M = 100 #number of maps to be made.
+N = 3 #number related to wakes
+string_tension = 10**(-7)
+
+def max_file_num(path):
+    files = os.listdir(path)
+    if len(files)==0 : 
+        return 0
+    else : 
+        start = max([int(re.findall('[0-9]+', file)[0]) for file in files])+1
+    
+    return start
+
 
 def main():
-    for i in range(N):
-        signal_map = generate_signal(N=3, string_tension=10**-7)
+    start = max_file_num(path)
+    
+    for i in range(start, start+M):
+        signal_map = generate_signal(N=N, string_tension=string_tension)
         
         filename = "signal"+str(i)+".npy"
         
